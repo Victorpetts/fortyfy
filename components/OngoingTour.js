3616584e6e9efef809fbdabcbbb9da42e14570e3
Victor Pettersson <vike.pet@hotmail.com>
2019-01-28 13:21:57 +0100

    gjorde så att en turnering hämtar sin win condition från databasen och displayar den

--- components/OngoingTour.js
+++ components/OngoingTour.js
@@ -9,8 +9,8 @@ import {
 import style from '../assets/Style.js';
 import { Participants } from './Participants.js';
 
-class OngoingTour extends Component {
 
+class OngoingTour extends Component {
 
   mapPartic() {
     return this.props.partic.map((player) => {
@@ -26,15 +26,29 @@ class OngoingTour extends Component {
 
     render() {
 
-      const nameToFilter = this.props.tourName;
-      const filterItems = (e) => {
-        return this.props.tours.filter((el) => el.nameToFilter === e);
+      const thisToursName = this.props.tourName;
+      const filter = this.props.tours.find( thisTour => thisTour.name === thisToursName );
+      let winconText;
+
+      switch(filter.wincon) {
+        case '1':
+          winconText = 'Överlevt flest minuter';
+          break
+        case '2':
+          winconText = 'Flest sammanlagda kills';
+          break
+        case '3':
+          winconText = 'Flest placeringar i top 5';
+          break
+        default:
+          winconText = '';
+          break
       }
 
         return (
             <View style={style.container}>
                 <Text style={style.headerText}>Seger villkor:</Text>
-                <Text style={style.headerText}>{filterItems(nameToFilter)}</Text>
+                <Text style={style.headerText}>{winconText}</Text>
                 <Text style={style.paragraphText}>{this.props.tourName}</Text>
                 <ScrollView style={{ height: '100%' }}>
                   {this.mapPartic()}
